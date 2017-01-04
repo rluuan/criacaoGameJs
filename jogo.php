ab5ecfebf4434b9718ba972c2c9dc2df1e5f5f4a
rluuan <rodrigoluanx@gmail.com>
2017-01-04 21:04:59 -0200

    Create jogo.php

---
+++ jogo.php
@@ -0,0 +1,13 @@
+  <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script  src='js/jquery-3.1.1.min.js'></script>
+	<script src='js/myjs.js'></script>
+	<style src='css/style.css'></style>
+</head>
+<body onload="start()">
+	<img src="imgs/unnamed.png" id="jogador1" width="200px" height="200px" style="top:50px;position: absolute;">
+	<img src='imgs/dp.png' id='fogo' width='100px' height='100px' style='position:absolute;display: none;left:200px' class='c_fogo' />
+</body>
+</html>
